d4d1a74be1a7f45bcf53c1261a5cf622c5b8cd90
auto_jenkins_wlan5 <auto_jenkins_wlan5@realsil.com.cn>
2026-04-21 01:49:38 +0800

    update sdk
    sdk/component/ui version: 0bc01a0
    1. feat: add neon for smart ca32
    2. refactor: add cst328 to common-ts, add touch demo

--- LVGL/lvgl-9.3/demos/touch.c
+++ LVGL/lvgl-9.3/demos/touch.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Realtek Semiconductor Corp.
+ * Copyright (c) 2026 Realtek Semiconductor Corp.
  * All rights reserved.
  *
  * Licensed under the Realtek License, Version 1.0 (the "License");
@@ -13,56 +13,103 @@
  * limitations under the License.
  */
 
- #include <string.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include "touch.h"
+#include "ameba_soc.h"
+#include "os_wrapper.h"
+
+#include "lvgl.h"
+#include "lv_ameba_hal.h"
+#include "demos/lv_demos.h"
+#include "display_mode_setting.h"
 #include "input.h"
 
-static input_touch_data_t g_touch = { 0 };
-static input_key_data_t g_key = { 0 };
-static input_mouse_data_t g_mouse = { 0 };
+#define TOUCH_TAG "TOUCH"
+#define DEMO_TOUCH "cst328"
 
-void input_event_callback(input_event_t *event)
+static lv_indev_data_t g_lvgl_touch_data = {
+    .state = LV_INDEV_STATE_RELEASED,
+    .point = {0, 0}
+};
+
+static void touch_event_callback(input_event_t *event)
 {
-    switch (event->type) {
-        case INPUT_EVENT_TOUCH:
-            memcpy(&g_touch, &event->data.touch, sizeof(input_touch_data_t));
-            break;
-        case INPUT_EVENT_KEY:
-            memcpy(&g_key, &event->data.key, sizeof(input_key_data_t));
-            break;
-        case INPUT_EVENT_MOUSE:
-            memcpy(&g_mouse, &event->data.mouse, sizeof(input_mouse_data_t));
-            break;
-        default:
-            break;
+    if (event->type != INPUT_EVENT_TOUCH) {
+        return;
+    }
+
+    g_lvgl_touch_data.point.x = event->data.touch.x;
+    g_lvgl_touch_data.point.y = event->data.touch.y;
+    g_lvgl_touch_data.state = event->data.touch.pressed ?
+                              LV_INDEV_STATE_PRESSED :
+                              LV_INDEV_STATE_RELEASED;
+
+    if (event->data.touch.pressed) {
+        RTK_LOGD(TOUCH_TAG, "PRESS   (%3d, %3d)\n",
+                 event->data.touch.x, event->data.touch.y);
+    } else {
+        RTK_LOGD(TOUCH_TAG, "RELEASE (%3d, %3d)\n",
+                 event->data.touch.x, event->data.touch.y);
     }
 }
 
-bool touch_init(void)
-{
-    input_device_t *dev = NULL;
-    input_manager_init();
-    input_set_global_callback(input_event_callback);
-    dev = input_get_device("gt911");
-
-    if (dev) {
-        dev->ops.init();
-        dev->ops.enable();
-    }
-
-    return dev != NULL;
-}
-
-void touch_read(lv_indev_t *indev, lv_indev_data_t *data)
+static void touch_read_cb(lv_indev_t *indev, lv_indev_data_t *data)
 {
     (void)indev;
-    data->point.x = g_touch.x;
-    data->point.y = g_touch.y;
 
-    if (g_touch.pressed) {
-        data->state = LV_INDEV_STATE_PR;
-    } else {
-        data->state = LV_INDEV_STATE_REL;
+    data->point.x = g_lvgl_touch_data.point.x;
+    data->point.y = g_lvgl_touch_data.point.y;
+    data->state = g_lvgl_touch_data.state;
+}
+
+void touch_init(void)
+{
+    RTK_LOGI(TOUCH_TAG, "===== Touch Initialization =====\n");
+
+    input_manager_init();
+    input_set_global_callback(touch_event_callback);
+
+    input_device_t *ts = input_get_device(DEMO_TOUCH);
+    if (!ts) {
+        RTK_LOGE(TOUCH_TAG, "ts(%s) device not found!\n", DEMO_TOUCH);
+        return;
     }
+
+    RTK_LOGI(TOUCH_TAG, "Found ts(%s) device\n", DEMO_TOUCH);
+
+    if (ts->info.state == INPUT_DEV_DISABLED) {
+        if (ts->ops.init) {
+            ts->ops.init();
+            RTK_LOGI(TOUCH_TAG, "ts(%s) hardware initialized\n", DEMO_TOUCH);
+        }
+    }
+
+    if (ts->info.state != INPUT_DEV_ENABLED) {
+        if (ts->ops.enable) {
+            ts->ops.enable();
+            ts->info.state = INPUT_DEV_ENABLED;
+            RTK_LOGI(TOUCH_TAG, "ts(%s) enabled\n", DEMO_TOUCH);
+        }
+    }
+
+    RTK_LOGI(TOUCH_TAG, "ts(%s) ready (state: %d)\n", DEMO_TOUCH, ts->info.state);
+    RTK_LOGI(TOUCH_TAG, "===== Touch Ready =====\n");
+}
+
+int touch_register_to_lvgl(void)
+{
+    RTK_LOGI(TOUCH_TAG, "Registering touch to LVGL...\n");
+
+    lv_indev_t *indev_touch = lv_indev_create();
+    if (!indev_touch) {
+        RTK_LOGE(TOUCH_TAG, "Failed to create input device!\n");
+        return -1;
+    }
+
+    lv_indev_set_type(indev_touch, LV_INDEV_TYPE_POINTER);
+    lv_indev_set_read_cb(indev_touch, touch_read_cb);
+
+    RTK_LOGI(TOUCH_TAG, "Touch registered to LVGL\n");
+    return 0;
 }
