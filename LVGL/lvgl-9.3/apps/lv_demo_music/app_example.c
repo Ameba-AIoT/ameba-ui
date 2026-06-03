58e9809cd600c92b7bd2f838d13a7225037f905f
auto_jenkins_wlan5 <auto_jenkins_wlan5@realsil.com.cn>
2026-06-04 03:38:04 +0800

    update sdk
    sdk/component/ui version: 6a07855
    1. refactor: lvgl hal for amebagreen2 jpeg interface
    2. refactor: refactor lvgl porting code directory
    3. fix: screen flickering on smart mipi panel.
    4. fix(libpng): need libpng NEON for image-decoder in amebasmart
    5. feat(graphics): add image decoder
    6. feat: add qoi
    7. refactor: refactor ui frameworks directory

---
+++ LVGL/lvgl-9.3/apps/lv_demo_music/app_example.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2026 Realtek Semiconductor Corp.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "lv_demo_music.h"
+#include "lv_port.h"
+
+void app_example(void)
+{
+    lv_port_run_demo(lv_demo_music, 0);
+}
