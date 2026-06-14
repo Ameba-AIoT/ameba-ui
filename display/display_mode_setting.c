ee0a10130d195c810e0ec5dfb9069460b3e4aba1
auto_jenkins_wlan5 <auto_jenkins_wlan5@realsil.com.cn>
2026-06-15 02:48:44 +0800

    update sdk
    sdk/component/ui version: fa2cb0b
    1. fix(mipi): Set a common D-PHY voltage settling delay.
    2. feat(ts): add touchscreen tddi
    3. refactor(panel): move panel config to display.
    4. fix(mipi): defer MIPI_DSI_Mode_Switch to first page flip to prevent dummy display
    5. feat(panel): add panel st7102
    6. refactor: can config lane count
    7. refactor: size of mipi cmd (max: 64)

--- display/display_mode_setting.c
+++ display/display_mode_setting.c
@@ -31,7 +31,9 @@
 
 #define DEBUG_PURE 0
 
-#if defined(CONFIG_ST7701S_MIPI) && CONFIG_ST7701S_MIPI
+#if defined(CONFIG_ST7102_MIPI) && CONFIG_ST7102_MIPI
+#define SELECTED_PANEL_NAME "st7102_mipi_480x480"
+#elif defined(CONFIG_ST7701S_MIPI) && CONFIG_ST7701S_MIPI
 #define SELECTED_PANEL_NAME "st7701s_mipi_480x800"
 #elif defined(CONFIG_ST7701S) && CONFIG_ST7701S
 #define SELECTED_PANEL_NAME "st7701s_rgb_800x480"
@@ -89,7 +91,9 @@ void fillPureBlueBuffer(uint32_t* buffer, int total_pixels) {
 void display_mode_pure_color(int32_t color_depth) {
     size_t buffer_size = 0;
     size_t total_pixels = 0;
-    if (!strcmp(SELECTED_PANEL_NAME, "st7701s_mipi_480x800")) {
+    if (!strcmp(SELECTED_PANEL_NAME, "st7102_mipi_480x480")) {
+        total_pixels = 480 * 480;
+    } else if (!strcmp(SELECTED_PANEL_NAME, "st7701s_mipi_480x800")) {
         total_pixels = 480 * 800;
     } else if (!strcmp(SELECTED_PANEL_NAME, "st7701s_rgb_800x480")) {
         total_pixels = 800 * 480;
@@ -139,6 +143,10 @@ bool display_mode_init(int32_t color_depth) {
     panel_configure_pinmux(SELECTED_PANEL_NAME);
 
     // 2. register all panels available.
+
+    extern bool panel_st7102_mipi_register(void);
+    panel_st7102_mipi_register();
+
     extern bool panel_st7701s_mipi_register(void);
     panel_st7701s_mipi_register();
 
